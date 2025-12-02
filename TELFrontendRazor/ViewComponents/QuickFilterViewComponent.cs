18e20f75f0c1082df499dfcf74d649a8ad7f3a88
akdalin <dalin.akrasi@nhs.net>
2025-12-02 09:37:15 +0000

    Implement QuickFilters

---
+++ TELFrontendRazor/ViewComponents/QuickFilterViewComponent.cs
@@ -0,0 +1,29 @@
+﻿namespace TELFrontendRazor.ViewComponents
+{
+    using Microsoft.AspNetCore.Mvc;
+    using TELFrontendRazor.ViewModels;
+
+    /// <summary>
+    /// A ViewComponent that renders a tag based on the provided name and styling.
+    /// </summary>
+    public class QuickFiltersViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(
+            List<string> tagNames,
+            string myLearningsType,
+            string resourcesType,
+            string cataloguesType,
+            string dashboardTrayLearningResourceType)
+        {
+            var model = new QuickFiltersViewModel
+            (
+                tagNames,
+                myLearningsType,
+                resourcesType,
+                cataloguesType,
+                dashboardTrayLearningResourceType
+            );
+            return View(model);
+        }
+    }
+}
---
+++ TELFrontendRazor/ViewModels/QuickFiltersViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace TELFrontendRazor.ViewModels
+{
+    public class QuickFiltersViewModel
+    {
+        public QuickFiltersViewModel(
+            List<string> tagNames,
+            string myLearningsType,
+            string resourcesType,
+            string cataloguesType,
+            string dashboardTrayLearningResourceType)
+        {
+            TagNames = tagNames;
+            MyLearningsType = myLearningsType;
+            ResourcesType = resourcesType;
+            CataloguesType = cataloguesType;
+            DashboardTrayLearningResourceType = dashboardTrayLearningResourceType;
+        }
+
+        public List<string> TagNames { get; set; }
+
+        /// <summary>
+        /// Gets or sets a type of my learning items to be displayed in the dashboard.
+        /// </summary>
+        public string MyLearningsType { get; set; }
+
+        /// <summary>
+        /// Gets or sets a type of resources to be displayed in the dashboard.
+        /// </summary>
+        public string ResourcesType { get; set; }
+
+        /// <summary>
+        /// Gets or sets a type of catalogues to be displayed in the dashboard.
+        /// </summary
+        public string CataloguesType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the dashboard tray Learning resource type.
+        /// </summary>
+        public string DashboardTrayLearningResourceType { get; set; }
+    }
+}
